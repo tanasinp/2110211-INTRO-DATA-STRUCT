ff0a05ccd9013c95936aafbbb3b21e29d102f88c
tanasinp <wavet888@gmail.com>
2023-09-07 14:44:53 +0700

    100 min_max

--- exercise/d65_q1c_min_of_max.cpp
+++ exercise/d65_q1c_min_of_max.cpp
@@ -8,12 +8,13 @@ using namespace std;
 int a[500112];
 
 int main(){
+    fill_n(a,500112,1);
     std::ios_base::sync_with_stdio(false); std::cin.tie(0);
     int n,m;
     cin >> n >> m;
 
-    map <int,int> mappo;
-    multiset<int> multi = {1};
+    map <int, vector<int>> mappo;
+    multiset<int> multi ;
     vector<int> v;
 
     for (int i = 0 ; i < n ; i++){
@@ -22,12 +23,44 @@ int main(){
         v.push_back(num);
     }
 
-    for (int i=0 ; i < m ; i++){
+    for (int i = 0 ; i < m; i++){
+        multi.insert(1);
+    }
+
+    for (int i=0 ; i < n ; i++){
         int index;
         cin >> index;
 
-        a[index] = v[i];
-        
+        // if (a[index] == 0){
+        //     a[index] = v[i];
+        //     multi.insert(a[index]);
+        // } else {
+        //     if (v[i] > a[index]){
+        //         auto it2 = multi.find(a[index]);
+        //         multi.erase(it2);
+
+        //         a[index] = v[i];
+        //         multi.insert(a[index]);
+
+
+        //     }
+        // }
+
+        if (v[i] > a[index]){
+            auto it2 = multi.find(a[index]);
+            multi.erase(it2);
+
+            a[index] = v[i];
+            multi.insert(a[index]);
+
+        }
+
+        // for (auto x : multi){
+        //     cout << x << " ";
+        // }
+        auto it = multi.begin();        
+        cout << *it << " ";
     }
+    cout << endl;
 
 }
