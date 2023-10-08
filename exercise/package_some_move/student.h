fa4c20bd765b0944c62d960b48dde2a42a358b65
tanasinp <wavet888@gmail.com>
2023-10-08 21:12:38 +0700

    40 some move

--- exercise/package_some_move/student.h
+++ exercise/package_some_move/student.h
@@ -9,13 +9,32 @@
 template <typename T>
 void CP::vector_some_move<T>::insert(int index, std::vector<T> &value) {
   // Your code here
-  mSize++;
-  ensureCapacity(mSize);
-  for (int i = mSize-1 ; i > index ; i--){
-    mData[i] = mData[i-1];
+  int colum,row;
+  auto it = std::upper_bound(aux.begin(),aux.end(),index);
+  if (it == aux.begin()){
+    colum = index;
+  } else {
+    colum = index - *(it-1);
   }
-  mData[index] = value;
+  row = it - aux.begin();
+
+  if (colum == 0){
+    mData.insert(mData.begin()+row,value);
+    aux.insert(aux.begin()+row,value.size());
+    for (int i = row + 1 ; i < aux.size() ; i++){
+      aux[i] += value.size();
+    }
+  } else {
+    mData[row].insert(mData[row].begin()+colum, value.begin(), value.end());
+    for (int i = row ; i < aux.size() ; i++){
+      aux[i] += value.size();
+    }
+  }
+  mCap += value.size();
+  mSize += value.size();
   
+
+
 }
 
 #endif
