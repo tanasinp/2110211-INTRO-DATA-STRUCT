2d8b0319b88a3a47863be7b60ad5e9a96e57596d
tanasinp <wavet888@gmail.com>
2023-10-14 20:18:39 +0700

    .

---
+++ exercise/d62_q3a_heap_node_relation/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin >> n >> m;
+    while(m--){
+        int a,b;
+        cin >> a >> b;
+        if (a < b){
+            while (a < b){
+                b = (b-1)/2;
+            }
+            if (a == b){
+                cout << "a is an ancestor of b\n";
+            } else {
+                cout << "a and b are not related\n";
+            }
+        } else if (a > b){
+            while (a > b){
+                a = (a-1)/2;
+            }
+            if (a == b){
+                cout << "b is an ancestor of a\n";
+            } else {
+                cout << "a and b are not related\n";
+            }
+        } else {
+            cout << "a and b are the same node\n";
+        }
+    }
+}
