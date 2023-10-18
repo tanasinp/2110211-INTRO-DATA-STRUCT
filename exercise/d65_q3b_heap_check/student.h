25808654545cbcce25b29ad716c7fd6bd0f0443a
tanasinp <wavet888@gmail.com>
2023-10-19 01:18:31 +0700

    pq

---
+++ exercise/d65_q3b_heap_check/student.h
@@ -0,0 +1,13 @@
+#ifndef __STUDENT_H
+#define __STUDENT_H
+
+#include "priority_queue.h"
+
+template <typename T, typename Comp>
+bool CP::priority_queue<T, Comp>::check() {
+  // Your code here
+  // Return something
+  return true;
+}
+
+#endif
