2e533b714138438d32639d06f1f9518e1a84ccb7
tanasinp <wavet888@gmail.com>
2023-11-09 18:14:37 +0700

    list 70

--- exercise/d57_q3_shift/shift.h
+++ exercise/d57_q3_shift/shift.h
@@ -1,4 +1,25 @@
 void shift(int k) {
 	// TODO: Add your code here
+	k %= mSize;
+	node *pos = mHeader;
+	if (k == 0) return;
+	if (k<0){
+		k = (k+mSize) % mSize;
+	}
+	for (int i = 0 ; i < k ; i++){
+		pos = pos->next;
+	}
+	node *st = mHeader->next;
+	node *ed = mHeader->prev;
+
+	st->prev = ed;
+	ed->next = st;
+
+	mHeader->next = pos->next;
+	mHeader->next->prev = mHeader;
+
+	pos->next = mHeader;
+	mHeader->prev = pos;
 
 }
+
