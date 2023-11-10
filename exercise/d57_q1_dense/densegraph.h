172e2b7b8bb4d2e7e2c6caa26f5bf148e30fbdf3
tanasinp <wavet888@gmail.com>
2023-11-10 10:34:17 +0700

    graph

--- exercise/d57_q1_dense/densegraph.h
+++ exercise/d57_q1_dense/densegraph.h
@@ -6,36 +6,51 @@
 using namespace std;
 class DenseGraph{
 public:
-    DenseGraph() {
+    DenseGraph() : DenseGraph(3) {
         // Your code here
     }
 
-    DenseGraph(int n_in) {
+    DenseGraph(int n_in) : n(n_in) , al(n_in) {
         // Your code here
+        for (auto &x : al){
+            x.resize(n_in);
+        }
     }
 
-    DenseGraph(const DenseGraph& G) {
+    DenseGraph(const DenseGraph& G) : n(G.n) , al(G.al) {
         // Your code here
+
     }
 
     void AddEdge(int a, int b) {
         // Your code here
+        al[a][b] = true;
     }
 
     void RemoveEdge(int a, int b) {
         // Your code here
+        al[a][b] = false;
     }
 
     bool DoesEdgeExist(int a, int b) const {
         // Your code here
+        return al[a][b];
     }
 
     DenseGraph Transpose() const {
         // Your code here
+        DenseGraph res(n);
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < n ; j++){
+                res.al[j][i] = al[i][j];
+            }
+        }
+        return res;
     }
 
 protected:
     int n;
     // Your code here
+    vector<vector<bool>> al;
 };
 #endif // __DENSE_GRAPH_H__
