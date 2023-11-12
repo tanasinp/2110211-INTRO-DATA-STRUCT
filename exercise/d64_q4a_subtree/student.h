fc40d32a8d705bb33d16a1458a1c9313a2209b88
tanasinp <wavet888@gmail.com>
2023-11-12 10:42:46 +0700

    bst

--- exercise/d64_q4a_subtree/student.h
+++ exercise/d64_q4a_subtree/student.h
@@ -8,13 +8,30 @@
 template <typename KeyT,typename MappedT, typename CompareT >
 size_t CP::map_bst<KeyT,MappedT,CompareT>::process(node* n) {
   //write your code here
-  return 0;
+  if (n == NULL) return 0;
+  return 1 + process(n->left) + process(n->right);
 }
 
 template <typename KeyT,typename MappedT, typename CompareT >
 std::pair<KeyT,MappedT> CP::map_bst<KeyT,MappedT,CompareT>::subtree(map_bst<KeyT,MappedT,CompareT> &left, map_bst<KeyT,MappedT,CompareT> &right) {
   //write your code here
-  return std::pair<KeyT,MappedT>();
+  if (mRoot == NULL){
+    return std::pair<KeyT,MappedT>();
+  }
+  left.mSize = process(mRoot->left);
+  right.mSize = process(mRoot->right);
+  
+  left.mRoot = mRoot->left;
+  right.mRoot = mRoot->right;
+
+  mRoot->left = NULL;
+  mRoot->right = NULL;
+
+  this->mSize = 1;
+
+  return mRoot->data;
+  
+  
 }
 
 #endif
