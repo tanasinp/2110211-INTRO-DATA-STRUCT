4786adc8d817aec0ef169e671266ed36b8b61550
tanasinp <wavet888@gmail.com>
2023-11-14 00:56:17 +0700

    .

--- exercise/d59_q3_bst_split/student.h
+++ exercise/d59_q3_bst_split/student.h
@@ -7,7 +7,29 @@ template <typename KeyT,
 CP::map_bst<KeyT,MappedT,CompareT> CP::map_bst<KeyT,MappedT,CompareT>::split(KeyT val) {
   //your code here
   CP::map_bst<KeyT,MappedT,CompareT> result;
-  
+  node* p = NULL;
+  node* now = mRoot;
+
+  while (now != NULL){
+    int cmp = compare(val, now->data.first);
+    if (cmp == 0 || cmp == -1) {
+      break;
+    }
+    p = now;
+    now = now->right;
+  }
+  result.mRoot = now;
+  if (p == NULL){
+    node* tmp = mRoot->left;
+    mRoot->left = NULL;
+    mRoot = tmp;
+  } else {
+    if (p->left->data.first == now->data.first) {
+      p->left = NULL;
+    } else {
+      p->right = NULL;
+    }
+  }
   return result;
 }
 
