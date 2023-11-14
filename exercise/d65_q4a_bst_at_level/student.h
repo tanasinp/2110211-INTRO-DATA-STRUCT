7b8bda3d24dbb31756e556f8a9100e8cd2e02bc7
tanasinp <wavet888@gmail.com>
2023-11-14 18:24:45 +0700

    .

---
+++ exercise/d65_q4a_bst_at_level/student.h
@@ -0,0 +1,23 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+#include <iostream>
+
+// you can modify and use this function
+template <typename KeyT,
+          typename MappedT,
+          typename CompareT>
+void CP::map_bst<KeyT,MappedT,CompareT>::my_recur(node* n,size_t level,size_t tmp,std::vector<KeyT> &v) {
+  //you MAY need to use this function
+}
+
+template <typename KeyT,
+          typename MappedT,
+          typename CompareT>
+std::vector<KeyT> CP::map_bst<KeyT,MappedT,CompareT>::at_level(size_t level) {
+  //write your code here
+}
+
+
+#endif
