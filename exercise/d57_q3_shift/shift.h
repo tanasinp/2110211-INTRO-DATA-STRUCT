6f34d48d1295d8d9bcf7ac3c61420f2294675a96
tanasinp <wavet888@gmail.com>
2023-11-16 09:51:36 +0700

    bst

--- exercise/d57_q3_shift/shift.h
+++ exercise/d57_q3_shift/shift.h
@@ -1,16 +1,48 @@
 void shift(int k) {
 	// TODO: Add your code here
-	k %= mSize;
-	node *pos = mHeader;
-	if (k == 0) return;
-	if (k<0){
-		k = (k+mSize) % mSize;
+	//first
+	// if(mSize == 1)return;
+	// if (k > 0){
+	// 	for (int i = 0; i < k ;i++){
+	// 		node* st = mHeader->next;
+	// 		node* ed = mHeader->prev;
+	// 		mHeader->next = st->next;
+	// 		mHeader->next->prev = mHeader;
+	// 		ed->next = st;
+	// 		ed->next->prev = ed;
+	// 		st->next = mHeader;
+	// 		mHeader->prev = st;
+	// 	}
+	// }else {
+	// 	for (int i = 0 ; i > k ; i--){
+	// 		node* st = mHeader->next;
+	// 		node* ed = mHeader->prev;
+	// 		ed->prev->next = mHeader;
+	// 		mHeader->prev = ed->prev;
+	// 		mHeader->next = ed;
+	// 		ed->prev = mHeader;
+	// 		ed->next = st;
+	// 		st->prev = ed;	
+	// 	}
+	// }
+
+	//second
+	int n;
+	if (k < 0){
+	  n = k;
+	  while(n < 0){
+	    n+=mSize;
+	  }
+	} else {
+	  n = k%mSize;
 	}
-	for (int i = 0 ; i < k ; i++){
+	node* pos = mHeader;
+	if (n == 0) return;
+	for (int i = 0 ; i < n ; i++){
 		pos = pos->next;
 	}
-	node *st = mHeader->next;
-	node *ed = mHeader->prev;
+	node* st = mHeader->next;
+	node* ed = mHeader->prev;
 
 	st->prev = ed;
 	ed->next = st;
@@ -19,7 +51,6 @@ void shift(int k) {
 	mHeader->next->prev = mHeader;
 
 	pos->next = mHeader;
-	mHeader->prev = pos;
-
+  	pos->next->prev = pos;
 }
 
